f4e0ee91dc1441c0d26f2f6cc229880be66e6a79
Aksarium <xekytok@gmail.com>
2021-08-19 19:11:54 +0300

    Добавьте файлы проекта.

---
+++ MyFirstrProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstrProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's display current system date & time");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
